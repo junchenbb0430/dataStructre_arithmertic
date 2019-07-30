6eafd67e2a0f29d99b782cdf14c7d0bd94718aa5
lijunshi <lijunshi@ehomepay.com.cn>
2019-07-30 14:32:52 +0800

    新增工程

---
+++ src/main/java/com/ljs/aritchmertic/DataStructreArithmerticApplication.java
@@ -0,0 +1,13 @@
+package com.ljs.aritchmertic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructreArithmerticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStructreArithmerticApplication.class, args);
+	}
+
+}
